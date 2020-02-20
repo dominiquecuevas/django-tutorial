c5a2b148168a6b35bd1627d8767152d233b8e862
Dominique Cuevas <cuevasdominique@gmail.com>
2020-02-19 23:19:10 -0800

    tested another mypage view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+# path(route, view[, kwargs, name])
+# route is string that contains URL pattern. doesn't search GET/POST params ie mysite.com/polls/?page=3
+# view calls the function from views.py with an HttpRequest object and any values from the route
+# name allows referencing
 urlpatterns = [
-    path('', views.index, name='index')
+    path('mypage', views.mypage, name='mypage'),
+    path('', views.index, name='index'),
 ]
