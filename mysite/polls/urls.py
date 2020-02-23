97d15c7adf91b9d20b4d0687abcafee1927d2dad
Dominique Cuevas <cuevasdominique@gmail.com>
2020-02-22 17:13:51 -0800

    write view functions, querying, templating, raise 404

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,7 +5,12 @@ from . import views
 # route is string that contains URL pattern. doesn't search GET/POST params ie mysite.com/polls/?page=3
 # view calls the function from views.py with an HttpRequest object and any values from the route
 # name allows referencing
+
+# namespacing to make sure django goes to the right view if apps share the same name
+app_name = 'polls'
 urlpatterns = [
-    path('mypage', views.mypage, name='mypage'),
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
